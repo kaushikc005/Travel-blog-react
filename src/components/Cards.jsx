11071fcf92e9a71d95b8876d99dc3ea470ec5dcd
kaushik <kaushikc005@gmail.com>
2023-06-01 19:26:02 +0530

    Misc commit

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import "./cssutils/Card.css";
+import "./cssutils/Cards.css"
 import CardItem from './CardItem'
 const Cards = () => {
   return (
